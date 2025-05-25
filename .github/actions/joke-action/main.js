b1cd1e68e63fafdf49ec0252b62be27b3380f4a3
TSAI PEI-SHAN <s506@kimo.com>
2025-05-25 16:58:46 +0800

    update to use environment file for set-output

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -1,8 +1,14 @@
 const getJoke = require("./joke");
+const fs = require("fs");
+const path = require("path");
 
 async function run() {
   const joke = await getJoke();
-  console.log(`::set-output name=joke-output::${joke}`);
+  console.log(joke);
+
+  // 正確寫入 GitHub Actions 輸出用的環境檔案
+  const githubOutputPath = process.env.GITHUB_OUTPUT;
+  fs.appendFileSync(githubOutputPath, `joke-output=${joke}\n`);
 }
 
 run();
