b9cca69c818179986055aa3acb13e03af1343da4
TSAI PEI-SHAN <s506@kimo.com>
2025-05-25 16:20:51 +0800

    replace request-promise with native https module

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -1,18 +1,40 @@
-const request = require("request-promise");
-
-const options = {
-  method: "GET",
-  uri: "https://icanhazdadjoke.com/",
-  headers: {
-    Accept: "application/json",
-    "User-Agent": "Writing JavaScript action GitHub Skills course.",
-  },
-  json: true,
-};
+const https = require("https");
 
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  const options = {
+    hostname: "icanhazdadjoke.com",
+    path: "/",
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "User-Agent": "Writing JavaScript action GitHub Skills course.",
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    const req = https.request(options, (res) => {
+      let data = "";
+
+      res.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      res.on("end", () => {
+        try {
+          const parsed = JSON.parse(data);
+          resolve(parsed.joke);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    });
+
+    req.on("error", (error) => {
+      reject(error);
+    });
+
+    req.end();
+  });
 }
 
 module.exports = getJoke;
